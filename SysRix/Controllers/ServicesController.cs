989de664b0cab89ef4e56b490481a0cb04a755b8
Richer Archambault <richer.arc@gmail.com>
2017-05-17 15:30:11 -0400

    Base project for SysRix

---
+++ SysRix/Controllers/ServicesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SysRix.Controllers
+{
+    [Route("api/[controller]")]
+    public class ServicesController : Controller
+    {
+        // GET api/users
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Server1", "address" };
+        }
+    }
+}
---
+++ SysRix/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SysRix.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        // GET api/users
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "name", "password" };
+        }
+    }
+}
